d27ca3f417b98abef141564b50080fdfbfe7e1bc
Bruno Gomes <icestonebruno@gmail.com>
2021-07-17 18:11:21 -0300

    cpf celular e email

--- public/scripts/actions.js
+++ public/scripts/actions.js
@@ -1,14 +1,148 @@
+const div_cpf = document.getElementById('div_cpf')
+const div_contato = document.getElementById('div_contato')
+const div_carreira = document.getElementById('div_carreira')
+const div_beneficio = document.getElementById('div_beneficio')
+
 function menu(){
   const menu_hamburger_abrir = document.querySelector('#menu-abrir')
   const menu_hamburger_fechar = document.querySelector('#menu-fechar')
   const menu = document.querySelector('.menu')
-  const body = document.querySelector('body')
 
   menu_hamburger_abrir.classList.toggle("disabled");
   menu_hamburger_fechar.classList.toggle("disabled");
   menu.classList.toggle("disabled");
 
+}
 
+div_cpf.classList.add('enable')
+div_contato.classList.add('disable')
+div_carreira.classList.add('disable')
+div_beneficio.classList.add('disable')
 
+function verifyPassQuestionButton1(){
+
+  const button1 = document.getElementById('proximo1')
+  const campoCpf = document.getElementById('cpf')
+
+  if(campoCpf.value.length === 14){
+    button1.disabled = false
+    campoCpf.style.border = '3px solid #61b161';
+  }else{
+    button1.disabled = true
+    campoCpf.style.border = '3px solid tomato';
+  }
+  
+}
+
+function passQuestionButton1(){
+  
+  div_cpf.classList.remove('enable')
+  div_cpf.classList.add('disable')
+  
+  
+  div_contato.classList.remove('disable')
+  div_contato.classList.add('enable')
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+function verifyPassQuestionButton2(){
+  const button2 = document.getElementById('proximo2')
+  const celular = document.getElementById('celular')
+  const email = document.getElementById('email')
+  const verifyEmail = validacaoEmail(email)
+  const msgemail = document.getElementById("msgemail")
+
+  if(verifyEmail === false){
+    msgemail.innerText = "Email inválido, por favor verifique"
+    email.style.border = '3px solid tomato';
+  }else if(verifyEmail === true){
+    email.style.border = '3px solid #61b161';
+    msgemail.innerText = ""
+  }
+
+
+  if(celular.value.length === 15 && celular.value[5] === '9' && verifyEmail === true){
+    button2.disabled = false
+    celular.style.border = '3px solid #61b161';
+    button2.innerText = "Próximo"
+  }else if(celular.value[5] !== '9'){
+    
+    button2.innerText = "O número do celular com o DDD precisa ter 11 dígitos"
+
+    button2.disabled = true
+    celular.style.border = '3px solid tomato';
+  }else{
+    button2.innerText = "Próximo"
+    celular.style.border = '3px solid tomato';
+    button2.disabled = true
+  }
+
+
+  
+}
+
+function passQuestionButton2(){
+  
+  div_contato.classList.remove('enable')
+  div_contato.classList.add('disable')
+  
+  
+  div_carreira.classList.remove('disable')
+  div_carreira.classList.add('enable')
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function accepted(){
+  const wanning = document.querySelector('footer')
+  wanning.classList.add('disabled')
+}
+
+function validacaoEmail(field) {
+  usuario = field.value.substring(0, field.value.indexOf("@"));
+  dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+  
+  if ((usuario.length >=1) &&
+      (dominio.length >=3) &&
+      (usuario.search("@")==-1) &&
+      (dominio.search("@")==-1) &&
+      (usuario.search(" ")==-1) &&
+      (dominio.search(" ")==-1) &&
+      (dominio.search(".")!=-1) &&
+      (dominio.indexOf(".") >=1)&&
+      (dominio.lastIndexOf(".") < dominio.length - 1)) {
+      return true
+    }
+    else{
+
+    return false
+    }
+  }
+
+
+
---
+++ public/scripts/mascaras.js
@@ -0,0 +1,145 @@
+function mask(o, f) {
+  setTimeout(function() {
+    var v = f(o.value);
+    if (v != o.value) {
+      o.value = v;
+    }
+  }, 1);
+}
+
+// MASCARA DO TELEFONE FIXO
+function mphone(v) {
+  var r = v.replace(/\D/g, "");
+  r = r.replace(/^0/, "");
+  if (r.length > 10) {
+    r = r.replace(/^(\d\d)(\d{4})(\d{5}).*/, "($1) $2-$3");
+  } else if (r.length > 5) {
+    r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+  } else if (r.length > 2) {
+    r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+  } else {
+    r = r.replace(/^(\d*)/, "($1");
+  }
+  return r;
+}
+
+
+
+
+
+// MASCARA PARA O CELULAR
+function mphoneC(v) {
+  var r = v.replace(/\D/g, "");
+  r = r.replace(/^0/, "");
+  if (r.length > 10) {
+    r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+  } else if (r.length > 5) {
+    r = r.replace(/^(\d\d)(\d{4})(\d{0,5}).*/, "($1) $2-$3");
+  } else if (r.length > 2) {
+    r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+  } else {
+    r = r.replace(/^(\d*)/, "($1");
+  }
+  return r;
+}
+
+
+// MASCARA PARA O CPF
+function mCPF(cpf){
+  cpf=cpf.replace(/\D/g,"")
+  cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+  cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+  cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+
+  return cpf
+}
+
+
+function mascaraData(val) {
+  var pass = val.value;
+  var expr = /[0123456789]/;
+
+  for (i = 0; i < pass.length; i++) {
+    // charAt -> retorna o caractere posicionado no índice especificado
+    var lchar = val.value.charAt(i);
+    var nchar = val.value.charAt(i + 1);
+
+    if (i == 0) {
+      // search -> retorna um valor inteiro, indicando a posição do inicio da primeira
+      // ocorrência de expReg dentro de instStr. Se nenhuma ocorrencia for encontrada o método retornara -1
+      // instStr.search(expReg);
+      if ((lchar.search(expr) != 0) || (lchar > 3)) {
+        val.value = "";
+      }
+
+    } else if (i == 1) {
+
+      if (lchar.search(expr) != 0) {
+        // substring(indice1,indice2)
+        // indice1, indice2 -> será usado para delimitar a string
+        var tst1 = val.value.substring(0, (i));
+        val.value = tst1;
+        continue;
+      }
+
+      if ((nchar != '/') && (nchar != '')) {
+        var tst1 = val.value.substring(0, (i) + 1);
+
+        if (nchar.search(expr) != 0)
+          var tst2 = val.value.substring(i + 2, pass.length);
+        else
+          var tst2 = val.value.substring(i + 1, pass.length);
+
+        val.value = tst1 + '/' + tst2;
+      }
+
+    } else if (i == 4) {
+
+      if (lchar.search(expr) != 0) {
+        var tst1 = val.value.substring(0, (i));
+        val.value = tst1;
+        continue;
+      }
+
+      if ((nchar != '/') && (nchar != '')) {
+        var tst1 = val.value.substring(0, (i) + 1);
+
+        if (nchar.search(expr) != 0)
+          var tst2 = val.value.substring(i + 2, pass.length);
+        else
+          var tst2 = val.value.substring(i + 1, pass.length);
+
+        val.value = tst1 + '/' + tst2;
+      }
+    }
+
+    if (i >= 6) {
+      if (lchar.search(expr) != 0) {
+        var tst1 = val.value.substring(0, (i));
+        val.value = tst1;
+      }
+    }
+  }
+
+  if (pass.length > 10)
+    val.value = val.value.substring(0, 10);
+  return true;
+}
+
+
+function formatarMoeda(elemento) {
+  var valor = elemento.value;
+
+  valor = valor + '';
+  valor = parseInt(valor.replace(/[\D]+/g, ''));
+  valor = valor + '';
+  valor = valor.replace(/([0-9]{2})$/g, ",$1");
+
+  if (valor.length > 6) {
+      valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+  }
+  
+  elemento.value = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });;
+  if(valor == 'NaN') elemento.value = '';
+}
+
